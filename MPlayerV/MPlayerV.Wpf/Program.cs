96d1102332723ab6f5db032fb3319dcf5f07a6f1
Starz0r <starz0rdesign@gmail.com>
2018-02-21 01:00:59 -0600

    Eto Template

---
+++ MPlayerV/MPlayerV.Wpf/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using Eto.Forms;
+
+namespace MPlayerV.Wpf
+{
+	class MainClass
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			new Application(Eto.Platforms.Wpf).Run(new MainForm());
+		}
+	}
+}
